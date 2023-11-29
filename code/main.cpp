243f743846c91321cc7184cb808279013a4faf6a
AlexLiCa <alexlin.carre@gmail.com>
2023-11-29 15:24:41 -0600

    -Menu juegos v1

--- code/main.cpp
+++ code/main.cpp
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "lib/headers/jugador.h"
+#include "lib/headers/juegos.h"
 
 using namespace std;
 
@@ -72,62 +73,105 @@ int menu()
     return opcion;
 }
 
+// int main()
+// {
+//     Jugador jugador;
+//     bool primer_tiro = true;
+
+//     if(!jugador.get_tiene_acceso()){
+//         jugador.iniciar_hilo();
+//         primer_tiro = false;
+//     }
+
+//     int opc = 0;
+
+//     do
+//     {
+//         opc = menu();
+//         switch (opc)
+//         {
+//         case 1:
+//             jugador.muestra_tablero(true);
+//             break;
+//         case 2:
+//             jugador.muestra_tablero(false);
+//             break;
+//         case 3:
+//             cout << endl;
+//             jugador.colocar_barco();
+//             break;
+//         case 4:
+//             // if(jugador.get_tablero_listo()){
+//                 if(jugador.get_tiene_acceso()){
+//                     if(!primer_tiro){
+//                         jugador.finalizar_hilo(false);
+//                     }
+                    
+//                     jugador.tirar();
+//                     cout << "Termine de Tirar" << endl;
+//                 }
+//                 else {
+//                     cout << "\nEsperando turno";
+//                 }
+//             // }
+//             // else {
+//             //     cout << "\nTermina de llenar tu tablero";
+//             // }
+
+//             primer_tiro = false;
+
+//             break;
+//         case 5:
+//             break;
+//         default:
+//             cout << "Opcion invalida." << endl;
+//             break;
+//         }
+
+//     } while (opc != 5);
+
+//     return 0;
+// }
+
+// main.cpp
+
+
 int main()
 {
-    Jugador jugador;
-    bool primer_tiro = true;
-
-    if(!jugador.get_tiene_acceso()){
-        jugador.iniciar_hilo();
-        primer_tiro = false;
-    }
-
-    int opc = 0;
+    Juegos manager;
+    int choice;
+    string name;
+    bool en_juego = false;
 
     do
     {
-        opc = menu();
-        switch (opc)
+        cout << "Menu:" << endl;
+        cout << "1. Crear semaforo" << endl;
+        cout << "2. Entrar al semaforo" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Seleccione una opcion: ";
+        cin >> choice;
+
+        switch (choice)
         {
         case 1:
-            jugador.muestra_tablero(true);
+            cout << "Ingrese el nombre del semaforo a crear: ";
+            cin >> name;
+            manager.createSemaphore(name);
             break;
         case 2:
-            jugador.muestra_tablero(false);
+            cout << "Ingrese el nombre del semaforo al que desea entrar: ";
+            cin >> name;
+            manager.enterSemaphore(name);
             break;
         case 3:
-            cout << endl;
-            jugador.colocar_barco();
-            break;
-        case 4:
-            // if(jugador.get_tablero_listo()){
-                if(jugador.get_tiene_acceso()){
-                    if(!primer_tiro){
-                        jugador.finalizar_hilo(false);
-                    }
-                    
-                    jugador.tirar();
-                    cout << "Termine de Tirar" << endl;
-                }
-                else {
-                    std::cout << "\nEsperando turno";
-                }
-            // }
-            // else {
-            //     std::cout << "\nTermina de llenar tu tablero";
-            // }
-
-            primer_tiro = false;
-
-            break;
-        case 5:
+            cout << "Saliendo del programa." << endl;
             break;
         default:
-            cout << "Opcion invalida." << endl;
-            break;
+            cout << "Opcion invalida. Intente de nuevo." << endl;
         }
 
-    } while (opc != 5);
+    } while (choice != 3);
 
     return 0;
 }
